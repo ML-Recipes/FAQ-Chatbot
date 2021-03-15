b5b4238a3ec1b37673d48948f7b71bba59542f08
Dacian Tamasan <tamasan.dacian@gmail.com>
2021-03-15 20:28:45 +0100

    Add function to call back-end rest endpoint

--- front-end/chatbot/src/app/services/chatbot.service.ts
+++ front-end/chatbot/src/app/services/chatbot.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppConfig } from '../app-config';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ChatbotService {
 
-  constructor() { }
+  chatbotData$: Observable<any>;
+  private chatbotDataSubject = new Subject<any>();
+
+  constructor(private http: HttpClient, private appConfig: AppConfig) {
+    this.chatbotData$ = this.chatbotDataSubject.asObservable();
+  }
+
+  send_message(data: any) {
+    return this.http.post(this.appConfig.apiUrl + "/api/bert/", data)
+  }
+
 }
