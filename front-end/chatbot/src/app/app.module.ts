ba81fa776d93e939aead4b741e1b72fa0a1f9b0f
Dacian Tamasan <tamasan.dacian@gmail.com>
2021-03-15 20:23:56 +0100

    Create chatbot component

--- front-end/chatbot/src/app/app.module.ts
+++ front-end/chatbot/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppConfig } from './app-config';
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatbotComponent
   ],
   imports: [
     BrowserModule,
